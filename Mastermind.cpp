4ea6a526b7639a4ae4f3c5ac8e50d38e068978d4
DharDhar30 <dharsen@udel.edu>
2026-09-11 11:44:59 -0400

    edit getUserGuess so it passes test cases such as too many digits or using other characters than numbers.

--- Mastermind.cpp
+++ Mastermind.cpp
@@ -11,7 +11,6 @@ Mastermind::Mastermind() {
 
     cout << "Welcome to MasterMind!" << endl;
     cout << "Can you guess the " << code_len << "-digit secret code? (Digits do not repeat)" << endl;
-
     playGame();
 }
 
@@ -20,7 +19,6 @@ Mastermind::Mastermind(bool de_bug) {
     max_num_attempts = 10;
     numCols = 7;
     debug = de_bug;
-
     cout << "Welcome to MasterMind!" << endl;
     cout << "Can you guess the " << code_len << "-digit secret code? (Digits do not repeat)" << endl;
 
@@ -28,7 +26,6 @@ Mastermind::Mastermind(bool de_bug) {
 }
 
 void Mastermind::makeSecretCode() {
-
     bool used[10] = {false};
     for (int i = 0; i < code_len; i++) {
         int num = rand() % 10;
@@ -39,16 +36,15 @@ void Mastermind::makeSecretCode() {
         used[num] = true;
     }
 }
-void Mastermind::calculateFeedback(int *RR, int *RW, int currGuess[]) {
-    *RR = 0;
-    *RW = 0;
+void Mastermind::calculateFeedback(int *LRR, int *LRW, int currGuess[]) {
+    *LRR = 0;
+    *LRW = 0;
     bool usedSecret[5] = {false};
     bool usedGuess[5] = {false};
-
     for (int i = 0; i < code_len; i++) {
 
         if (currGuess[i] == secretCode[i]) {
-            *RR = *RR + 1;
+            *LRR = *LRR + 1;
             usedSecret[i] = true;
             usedGuess[i] = true;
         }
@@ -58,7 +54,7 @@ void Mastermind::calculateFeedback(int *RR, int *RW, int currGuess[]) {
         if (usedGuess[i] == false) {
             for (int j = 0; j < code_len; j++) {
                 if (usedSecret[j] == false && usedGuess[i] == false && currGuess[i] == secretCode[j]) {
-                    *RW = *RW + 1;
+                    *LRW = *LRW + 1;
                     usedSecret[j] = true;
                     usedGuess[i] = true;
                 }
@@ -68,69 +64,61 @@ void Mastermind::calculateFeedback(int *RR, int *RW, int currGuess[]) {
 }
 
 void Mastermind::updateMatrix(int turn, const int currGuess[], int RR, int RW) {
-
     for (int i = 0; i < code_len; i++) {
         codeMatrix[turn][i] = currGuess[i];
     }
-
     codeMatrix[turn][5] = RR;
     codeMatrix[turn][6] = RW;
 }
 
 void Mastermind::printMatrix(int turn) {
-
     cout << endl;
     cout << "Guess Matrix:" << endl;
     for (int i = 0; i < turn; i++) {
         for (int j = 0; j < numCols; j++) {
             cout << codeMatrix[i][j] << " ";
         }
-
         cout << endl;
     }
     cout << endl;
 }
 
 void Mastermind::getUserGuess(int currGuess[]) {
-
     string input;
     bool getInput = true;
     while (getInput) {
         getInput = true;
         cout << "Enter your " << code_len << "-digit guess (e.g., 12345): ";
         cin >> input;
+
         if (input.length() != code_len) {
             cout << "The input is the wrong length. Please try again with "
-                 << code_len << " digits." << endl;
-
-            getInput = false;
-        }
-        bool validDigits = true;
-
-        for (int i = 0; i < code_len; i++) {
-
-            if (input[i] >= '0' && input[i] <= '9') {
-                currGuess[i] = input[i] - '0';
-            }
-            else {
-                validDigits = false;
-            }
-        }
-
-        cout << endl;
-
-        if (validDigits == false) {
-            cout << "Invalid input. Please only use numbers 0-9." << endl;
-            getInput = true;
+                 << code_len << " digits.\n";
         }
         else {
-            getInput = false;
+            bool validDigits = true;
+            for (int i = 0; i < code_len; i++) {
+                if (input[i] >= '0' && input[i] <= '9') {
+                    currGuess[i] = input[i] - '0';
+                }
+                else {
+                    validDigits = false;
+                }
+            }
+            cout << endl;
+
+            if (validDigits == false) {
+                cout << "Invalid input. Only use numbers 0-9." << endl;
+                getInput = true;
+            }
+            else {
+                getInput = false;
+            }
         }
     }
 }
 
 void Mastermind::playGame() {
-
     int currGuess[code_len];
     int RR;
     int RW;
@@ -143,7 +131,6 @@ void Mastermind::playGame() {
         for (int i = 0; i < code_len; i++) {
             cout << secretCode[i] << " ";
         }
-
         cout << endl;
     }
     while (won == false && turn < max_num_attempts) {
@@ -154,17 +141,13 @@ void Mastermind::playGame() {
         RW = 0;
 
         getUserGuess(currGuess);
-
         if (debug) {
-
             cout << "In debug: user's guess is ";
 
             for (int i = 0; i < code_len; i++) {
                 cout << currGuess[i] << " ";
             }
-
             cout << endl;
-
             cout << "Still in debug: secret code is ";
 
             for (int i = 0; i < code_len; i++) {
@@ -173,7 +156,6 @@ void Mastermind::playGame() {
             cout << endl;
         }
         calculateFeedback(&RR, &RW, currGuess);
-
         if (debug) {
             cout << "Debug in playGame: ";
             cout << "RR: " << RR << ", ";
@@ -182,17 +164,13 @@ void Mastermind::playGame() {
 
         updateMatrix(turn, currGuess, RR, RW);
         printMatrix(turn + 1);
-
         if (RR == code_len) {
-
             cout << "You cracked the code in "
                  << turn+1 << " turns!!!!" << endl;
-
             won = true;
         }
         turn++;
     }
-
     if (won == false) {
 
         cout << "Game Over! You ran out of turns. The secret code was: ";
